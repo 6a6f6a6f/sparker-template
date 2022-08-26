95dbb84251facb3506ef673407347e2055581475
Jojo <jonas.uliana@passwd.com.br>
2022-08-26 20:24:45 -0300

    chore: dunno :/

--- helpers/logcat.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "./core.sh"
-
-check_dependencies "$1"
-
-echo "Starting logcat"
-adb logcat "$1":I *:S
--- helpers/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-source "./core.sh"
-
-check_dependencies "$1"
-frida -U -f "$1" -l ../_agent.js --no-pause
